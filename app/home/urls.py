866d83c9c1d3896644d3db3ebecd9e313613fdb6
siddeshlc8 <siddeshlc8@gmail.com>
2018-09-07 23:53:39 +0530

    Home Page Related Pages Updated for The Project.

--- app/home/urls.py
+++ app/home/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('guest-house/', views.guest_house, name='guest_house'),
+    path('guest-rooms/', views.guest_room, name='guest_room'),
+    path('team/', views.team, name='team'),
+    path('admin-login/', views.admin_login, name='admin_login'),
+    path('news/', views.news, name='news'),
 ]

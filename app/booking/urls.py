ba0c75d08734cf30edd16adb181db0e58153e9f8
siddeshlc8 <siddeshlc8@gmail.com>
2018-09-12 13:50:07 +0530

    Booking Page Updated for The Project.

--- app/booking/urls.py
+++ app/booking/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('book/', views.book, name='book'),
 ]
--- app/booking/views.py
+++ app/booking/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def book(request):
+    return render(request, 'booking/book.html')
+

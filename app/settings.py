13850607f3ba646bda9639896224c65a3863447b
siddeshlc8@gmail.com <siddeshlc8@gmail.com>
2018-11-12 20:28:08 +0530

    Updated

--- app/settings.py
+++ app/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-MEDIA_ROOT= os.path.join(BASE_DIR, '/media')
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL= "/media/"
 
 

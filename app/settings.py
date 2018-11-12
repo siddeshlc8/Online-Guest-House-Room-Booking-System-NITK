df7356cdb45ac10b8026353be98781895fed4e1f
siddeshlc8@gmail.com <siddeshlc8@gmail.com>
2018-11-12 20:11:17 +0530

    Updated

--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2ddom#2bphyg(@h9q-lb_mzzo=7!k1p#)#2+o70bnyk@%#i5^='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['nitk-ghbs', '127.0.0.1','0.0.0.0', 'nitk-ghbs.herokuapp.com']
 
